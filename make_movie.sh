2e71c2aa67843534fa8a2612f341f4c9bfb2fded
toshiba <@>
2020-10-18 16:35:47 -0400

    add handbrake call to make_movie

--- make_movie.sh
+++ make_movie.sh
@@ -8,8 +8,11 @@ for i in $(ls -r -t ./images/*png); do
   x=$(($x+1))
 done
 
-FRAME_RATE=1
+FRAME_RATE=0.5
 VIDEO_FILENAME=timelapse_`date +%s`.mov
 
 # compile video
 ffmpeg -r $FRAME_RATE -i ./tmp/img%03d.png -b 15000k $VIDEO_FILENAME
+
+# compress video
+HandBrakeCLI -i $VIDEO_FILENAME -o $VIDEO_FILENAME.mkv
